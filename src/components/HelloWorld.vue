07348cd0c8fc82f247348eff022f9dc36438a17c
张名扬 <412891644@qq.com>
2020-03-15 19:23:06 +0800

    222

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="hello">
+		<formDemo v-if="showForm"></formDemo>
+		<button @click="showForm = true">显示</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'hello',
+	components:{
+		formDemo:()=>import('../components/child.vue')
+	},
+	props: {
+		msg: String
+	},
+	data(){
+		return{
+			showForm:false
+		}
+	}
+	
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+</style>
---
+++ src/components/child.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		异步组件
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'formDemo'
+	}
+</script>
+
+<style>
+</style>
